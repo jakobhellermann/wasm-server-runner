0bbc38627c2dec6a535cd64fc323efb195cb8d8c
Jakob Hellermann <jakob.hellermann@protonmail.com>
2021-09-20 21:24:27 +0200

    pick free port

--- src/server.rs
+++ src/server.rs
@@ -20,10 +20,10 @@ pub async fn run_server(output: WasmBindgenOutput) -> Result<()> {
         .route("/wasm.wasm", get(|| async { WithContentType("application/wasm", wasm) }))
         .layer(middleware_stack);
 
-    let port = 1334;
+    let port = pick_port::pick_free_port(1334, 10).unwrap_or(1334);
     let addr = SocketAddr::from(([127, 0, 0, 1], port));
 
-    tracing::info!("starting webserver at {}", addr);
+    tracing::info!("starting webserver at http://{}", addr);
     axum::Server::bind(&addr).serve(app.into_make_service()).await?;
 
     Ok(())
@@ -38,5 +38,31 @@ impl<T: IntoResponse> IntoResponse for WithContentType<T> {
         let mut response = self.1.into_response();
         response.headers_mut().insert("Content-Type", HeaderValue::from_static(self.0));
         response
+    }
+}
+
+mod pick_port {
+    use std::net::{Ipv4Addr, Ipv6Addr, SocketAddrV4, SocketAddrV6, TcpListener, ToSocketAddrs};
+
+    fn test_bind_tcp<A: ToSocketAddrs>(addr: A) -> Option<u16> {
+        Some(TcpListener::bind(addr).ok()?.local_addr().ok()?.port())
+    }
+    fn is_free_tcp(port: u16) -> bool {
+        let ipv4 = SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, port);
+        let ipv6 = SocketAddrV6::new(Ipv6Addr::UNSPECIFIED, port, 0, 0);
+
+        test_bind_tcp(ipv6).is_some() && test_bind_tcp(ipv4).is_some()
+    }
+
+    fn ask_free_tcp_port() -> Option<u16> {
+        let ipv4 = SocketAddrV4::new(Ipv4Addr::UNSPECIFIED, 0);
+        let ipv6 = SocketAddrV6::new(Ipv6Addr::UNSPECIFIED, 0, 0, 0);
+        test_bind_tcp(ipv6).or_else(|| test_bind_tcp(ipv4))
+    }
+
+    pub fn pick_free_port(starting_at: u16, try_consecutive: u16) -> Option<u16> {
+        (starting_at..=starting_at + try_consecutive)
+            .find(|&port| is_free_tcp(port))
+            .or_else(ask_free_tcp_port)
     }
 }
