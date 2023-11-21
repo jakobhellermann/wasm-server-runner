924a9c7a7ef3b6032899a321682842011a7de3c1
Jakob Hellermann <jakob.hellermann@protonmail.com>
2023-11-21 14:24:51 +0100

    just use tower compression

--- src/wasm_bindgen.rs
+++ src/wasm_bindgen.rs
@@ -1,13 +1,12 @@
 use crate::server::Options;
 use crate::Result;
-use anyhow::Context;
 use std::collections::HashMap;
 use std::path::Path;
 use tracing::debug;
 
 pub struct WasmBindgenOutput {
     pub js: String,
-    pub compressed_wasm: Vec<u8>,
+    pub wasm: Vec<u8>,
     pub snippets: HashMap<String, Vec<String>>,
     pub local_modules: HashMap<String, String>,
 }
@@ -35,22 +34,5 @@ pub fn generate(options: &Options, wasm_file: &Path) -> Result<WasmBindgenOutput
     let wasm = output.wasm_mut().emit_wasm();
     debug!("emitting wasm took {:?}", start.elapsed());
 
-    debug!("compressing wasm...");
-    let start = std::time::Instant::now();
-    let compressed_wasm = compress(&wasm).context("failed to compress wasm file")?;
-    debug!("compressing took {:?}", start.elapsed());
-
-    Ok(WasmBindgenOutput { js, compressed_wasm, snippets, local_modules })
-}
-
-fn compress(mut bytes: &[u8]) -> Result<Vec<u8>, std::io::Error> {
-    use brotli::enc::{self, BrotliEncoderParams};
-
-    let mut output = Vec::new();
-    enc::BrotliCompress(&mut bytes, &mut output, &BrotliEncoderParams {
-        quality: 5, // https://github.com/jakobhellermann/wasm-server-runner/pull/22#issuecomment-1235804905
-        ..Default::default()
-    })?;
-
-    Ok(output)
+    Ok(WasmBindgenOutput { js, wasm, snippets, local_modules })
 }
